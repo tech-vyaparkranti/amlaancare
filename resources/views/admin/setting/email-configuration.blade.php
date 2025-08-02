97f6e577f3f50f39495de4e263ad34c32263e157
vk-mithlesh <mithlesh.jha@vyaparkranti.in>
2025-08-02 16:17:13 +0530

    email configuration changes

--- resources/views/admin/setting/email-configuration.blade.php
+++ resources/views/admin/setting/email-configuration.blade.php
@@ -1,57 +1,62 @@
 <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">
     <div class="card border">
         <div class="card-body">
-            <form action="{{route('admin.email-setting-update')}}" method="POST">
+            @if(is_null($emailSettings))
+                <div class="alert alert-warning">
+                    No email settings found. Please enter and save to create new settings.
+                </div>
+            @endif
+
+            <form action="{{ route('admin.email-setting-update') }}" method="POST">
                 @csrf
                 @method('PUT')
+
                 <div class="form-group">
                     <label>Email</label>
-                    <input type="text" class="form-control" name="email" value="{{$emailSettings->email}}">
+                    <input type="email" class="form-control" name="email" value="{{ $emailSettings->email ?? '' }}" required>
                 </div>
 
                 <div class="form-group">
                     <label>Mail Host</label>
-                    <input type="text" class="form-control" name="host" value="{{$emailSettings->host}}">
+                    <input type="text" class="form-control" name="host" value="{{ $emailSettings->host ?? '' }}" required>
                 </div>
-
 
                 <div class="row">
                     <div class="col-md-6">
                         <div class="form-group">
-                            <label>Smtp username</label>
-                            <input type="text" class="form-control" name="username" value="{{$emailSettings->username}}">
+                            <label>SMTP Username</label>
+                            <input type="text" class="form-control" name="username" value="{{ $emailSettings->username ?? '' }}" required>
                         </div>
                     </div>
 
                     <div class="col-md-6">
                         <div class="form-group">
-                            <label>Smtp password</label>
-                            <input type="text" class="form-control" name="password" value="{{$emailSettings->password}}">
+                            <label>SMTP Password</label>
+                            <input type="password" class="form-control" name="password" value="{{ $emailSettings->password ?? '' }}" required>
                         </div>
                     </div>
                 </div>
 
-
                 <div class="row">
                     <div class="col-md-6">
                         <div class="form-group">
-                            <label>Mail port</label>
-                            <input type="text" class="form-control" name="port" value="{{$emailSettings->port}}">
+                            <label>Mail Port</label>
+                            <input type="text" class="form-control" name="port" value="{{ $emailSettings->port ?? '' }}" required>
                         </div>
                     </div>
 
                     <div class="col-md-6">
                         <div class="form-group">
                             <label>Mail Encryption</label>
-                            <select name="encryption" id="" class="form-control">
-                                    <option {{$emailSettings->encryption == 'tls' ? 'selected' : ''}} value="tls">TLS</option>
-                                    <option {{$emailSettings->encryption == 'ssl' ? 'selected' : ''}} value="ssl">SSL</option>
+                            <select name="encryption" class="form-control" required>
+                                <option value="tls" {{ ($emailSettings->encryption ?? '') === 'tls' ? 'selected' : '' }}>TLS</option>
+                                <option value="ssl" {{ ($emailSettings->encryption ?? '') === 'ssl' ? 'selected' : '' }}>SSL</option>
                             </select>
                         </div>
                     </div>
                 </div>
 
-                <button type="submit" class="btn btn-primary common_btn">Update</button>
+                <button type="submit" class="btn btn-primary common_btn mt-3">Update</button>
             </form>
         </div>
     </div>
